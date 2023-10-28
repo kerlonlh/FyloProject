518e26b95d600253d7aa2585783e055541a97e6c
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2023-10-28 17:25:28 -0300

    Update_js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
+const mobileNavbarItems = document.querySelectorAll('.navbar__mobile a');
 const button = document.querySelector('.burguer');
 
 button.addEventListener('click', function () {
     mobileNavbar.classList.toggle('active')
 })
+
+mobileNavbarItems.forEach( function (button){
+    button.addEventListener('click', function () {
+        mobileNavbar.classList.remove('active')
+    })
+})
+
 
 window.addEventListener('scroll', function () {
     if (this.window.pageYOffset > 0) return navbar.classList.add('active');
